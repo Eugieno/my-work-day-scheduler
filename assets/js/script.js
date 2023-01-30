dad379c878ea1e126ceee7eed513a455a4973e90
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-01-30 21:47:51 +0000

    code refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,51 +41,36 @@ for (var i = 0; i < timeArray.length; i++) {
 // function to save to local storage 
 function saveTostorage(event) {
     if (event) {
-        var textFromTextArea = $(this).parent().children('textarea').val().trim();
 
-        // var textFromTextArea = {
-        //     yourText: $(this).parent().children('textarea').val().trim(),
-        //     yourKey: $(this).attr('id'),
-        // } 
-        // var textER = textFromTextArea.yourKey
-        // var itemArr = localStorage.getItem(textER) || []
 
-        
-        // itemArr.push(textFromTextArea.yourText)
-
-        // console.log(typeof textFromTextArea)
-        // // itemArr.push(textFromTextArea)
-        // localStorage.setItem(textER, itemArr)
-        // // localStorage.setItem(textER, JSON.stringify(textFromTextArea.yourText))
         if (textFromTextArea == "") {
             return
         }
-        if ($(this).attr('id') == "button-1") {
-            var existingItems = localStorage.getItem('9AM-log') || []
+        if ($(this).attr('id')) {
+            // localStorage.getItem()
+
+            var textFromTextArea = $(this).parent().children('textarea').val().trim();
+            var accessKey = $(this).attr('id')
+            localStorage.setItem(accessKey, textFromTextArea);
             
-            localStorage.setItem("9AM-log", JSON.stringify(textFromTextArea))
-            
-        } else if ($(this).attr('id') == "button-2") {
-            localStorage.setItem("10AM-log", textFromTextArea)
-        } else if ($(this).attr('id') == "button-3") {
-            localStorage.setItem("11AM-log", textFromTextArea)
-        } else if ($(this).attr('id') == "button-4") {
-            localStorage.setItem("12PM-log", textFromTextArea)
-        } else if ($(this).attr('id') == "button-5") {
-            localStorage.setItem("1PM-log", textFromTextArea)
-        } else if ($(this).attr('id') == "button-6") {
-            localStorage.setItem("2PM-log", textFromTextArea)
-        } else if ($(this).attr('id') == "button-7") {
-            localStorage.setItem("3PM-log", textFromTextArea)
-        } else if ($(this).attr('id') == "button-8") {
-            localStorage.setItem("4PM-log", textFromTextArea)
-        } else {
-            localStorage.setItem("5PM-log", textFromTextArea)
-        // }
-        
-            
-        // $("[attribute=value]")
-        // console.log(textFromTextArea)
+        // } else if ($(this).attr('id') == "button-2") {
+        //     localStorage.setItem("10AM-log", textFromTextArea)
+        // } else if ($(this).attr('id') == "button-3") {
+        //     localStorage.setItem("11AM-log", textFromTextArea)
+        // } else if ($(this).attr('id') == "button-4") {
+        //     localStorage.setItem("12PM-log", textFromTextArea)
+        // } else if ($(this).attr('id') == "button-5") {
+        //     localStorage.setItem("1PM-log", textFromTextArea)
+        // } else if ($(this).attr('id') == "button-6") {
+        //     localStorage.setItem("2PM-log", textFromTextArea)
+        // } else if ($(this).attr('id') == "button-7") {
+        //     localStorage.setItem("3PM-log", textFromTextArea)
+        // } else if ($(this).attr('id') == "button-8") {
+        //     localStorage.setItem("4PM-log", textFromTextArea)
+        // } else {
+        //     localStorage.setItem("5PM-log", textFromTextArea)
+        }
+     
         
     }
     
@@ -93,7 +78,7 @@ function saveTostorage(event) {
  
 }
 
-}
+
 
 // Event Listener 
 $('.save').on('click', saveTostorage)

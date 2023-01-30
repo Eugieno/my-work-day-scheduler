d1af83af52edcf16e8a24d8144c670468a422408
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-01-30 23:53:53 +0000

    updated code that enforces timelog color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 var currentDayEl = $('#currentDay')
 
 // Current day - updated in the Header section
-
 var updateDate = function () {
     var currentDay = moment().format('dddd, MMMM Do')
     currentDayEl.text(currentDay)
@@ -14,17 +13,13 @@ setInterval(updateDate,1000)
 // Color coding 
 
 // Running hour
-var liveTime = setInterval(function() {
-    return moment().format('hA')
-},1000)
+// var liveTime = setInterval(function() {
+//     moment().format('hA')
+// },2000)
 
-// ................ I want to use liveTime (running hour) to test the conditon in line 27
-// ............... I'm not getting the expected value from the liveTime function above
-// .............  The color coding logic below has been tested for all time and works fine.
-// It's being tested at liveTime = '9AM' in its prsent form.
 var timeArray = $('.time')
 for (var i = 0; i < timeArray.length; i++) {
-    if (timeArray.eq(i).children().eq(0).text().trim() == '9AM') {
+    if (timeArray.eq(i).children().eq(0).text().trim() == moment().format('hA')) {
         
         
         timeArray.eq(i).children('textarea').css('background-color', '#D7263D')
@@ -41,34 +36,14 @@ for (var i = 0; i < timeArray.length; i++) {
 // function to save to local storage 
 function saveTostorage(event) {
     if (event) {
-
-
         if (textFromTextArea == "") {
             return
         }
         if ($(this).attr('id')) {
             // localStorage.getItem()
-
             var textFromTextArea = $(this).parent().children('textarea').val().trim();
             var accessKey = $(this).attr('id')
             localStorage.setItem(accessKey, textFromTextArea);
-            
-        // } else if ($(this).attr('id') == "button-2") {
-        //     localStorage.setItem("10AM-log", textFromTextArea)
-        // } else if ($(this).attr('id') == "button-3") {
-        //     localStorage.setItem("11AM-log", textFromTextArea)
-        // } else if ($(this).attr('id') == "button-4") {
-        //     localStorage.setItem("12PM-log", textFromTextArea)
-        // } else if ($(this).attr('id') == "button-5") {
-        //     localStorage.setItem("1PM-log", textFromTextArea)
-        // } else if ($(this).attr('id') == "button-6") {
-        //     localStorage.setItem("2PM-log", textFromTextArea)
-        // } else if ($(this).attr('id') == "button-7") {
-        //     localStorage.setItem("3PM-log", textFromTextArea)
-        // } else if ($(this).attr('id') == "button-8") {
-        //     localStorage.setItem("4PM-log", textFromTextArea)
-        // } else {
-        //     localStorage.setItem("5PM-log", textFromTextArea)
         }
      
         
@@ -77,8 +52,6 @@ function saveTostorage(event) {
     
  
 }
-
-
 
 // Event Listener 
 $('.save').on('click', saveTostorage)

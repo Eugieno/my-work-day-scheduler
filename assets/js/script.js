60fc861fb7e3f65e012be939bdcbfda59d3f1022
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-01-30 00:02:10 +0000

    added color code functionality to the timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,29 @@ var updateDate = function () {
     currentDayEl.text(currentDay)
     
 }
-
 setInterval(updateDate,1000)
+
+// Color coding 
+var timeArray = $('.time')
+var liveTime = setInterval(function() {
+    return moment().format('hA')
+},1000)
+console.log(moment().format('hA'))
+
+for (var i = 0; i < timeArray.length; i++) {
+    if (timeArray.eq(i).children().eq(0).text().trim() == liveTime) {
+        
+        
+        timeArray.eq(i).children('textarea').css('background-color', '#D7263D')
+
+        for (i+1 > i; i <timeArray.length; i++) {
+            timeArray.eq(i+1).children('textarea').css('background-color', '#8CD867')
+        } 
+    
+    } else {
+        timeArray.eq(i).children('textarea').css('background-color', '#D6D1CD')
+    }
+}
+
+// function to save to local storage 
+
